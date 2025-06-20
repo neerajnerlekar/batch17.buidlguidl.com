9fd12842709231761928897292174f0881cf4bba
Ankur Raj <gearhead@Ankurs-MacBook-Air.local>
2025-06-20 09:55:21 +0530

    changed the logo

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -83,11 +83,11 @@ export const Header = () => {
         </details>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="batch#17 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Batch #17 – Scaffold-ETH</span>
+            <span className="text-xs">Building the Decentralized Future</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
