4993ca135923fa72b11841d9fe8d1716c2f43c72
Ankur Raj <65127572+ankur-JA@users.noreply.github.com>
2025-06-25 10:45:08 +0530

    Update layout.tsx

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,7 +5,7 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Batch #17",
+  title: "Scaffold-ETH 2 App",
   description: "Built with 🏗 Scaffold-ETH 2",
 });
 
